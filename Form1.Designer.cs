4032995b8d65a03bd5c2ec71624af7f768c34025
Yusuf Taylan Yüksel <maestrofc27@gmail.com>
2019-01-07 15:26:57 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ideaFileReplacer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTargetFolder = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTargetFile = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnReplace = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // txtTargetFolder
+            // 
+            this.txtTargetFolder.Location = new System.Drawing.Point(99, 22);
+            this.txtTargetFolder.Name = "txtTargetFolder";
+            this.txtTargetFolder.Size = new System.Drawing.Size(526, 20);
+            this.txtTargetFolder.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hedef Klasör : ";
+            // 
+            // txtTargetFile
+            // 
+            this.txtTargetFile.Location = new System.Drawing.Point(99, 58);
+            this.txtTargetFile.Name = "txtTargetFile";
+            this.txtTargetFile.Size = new System.Drawing.Size(526, 20);
+            this.txtTargetFile.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Hedef Dosya :";
+            // 
+            // btnReplace
+            // 
+            this.btnReplace.Location = new System.Drawing.Point(99, 84);
+            this.btnReplace.Name = "btnReplace";
+            this.btnReplace.Size = new System.Drawing.Size(526, 45);
+            this.btnReplace.TabIndex = 4;
+            this.btnReplace.Text = "DEĞİŞTİR";
+            this.btnReplace.UseVisualStyleBackColor = true;
+            this.btnReplace.Click += new System.EventHandler(this.btnReplace_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(99, 135);
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(526, 431);
+            this.txtLog.TabIndex = 5;
+            this.txtLog.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(727, 597);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnReplace);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTargetFile);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTargetFolder);
+            this.Name = "Form1";
+            this.Text = "ideaFileReplacer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTargetFolder;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTargetFile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnReplace;
+        private System.Windows.Forms.RichTextBox txtLog;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ideaFileReplacer
+{
+    public partial class Form1 : Form
+    {
+
+        private string target_folder = "";
+        private string target_file = "";
+        private string file_name = "";
+
+        private int total_files_replaced = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void btnReplace_Click(object sender, EventArgs e)
+        {
+            ThreadStart ts = new ThreadStart(Replace);
+            Thread th = new Thread(ts);
+            th.Start();
+        }
+
+        private void Replace()
+        {
+            try
+            {
+                target_folder = "";
+                target_file = "";
+                file_name = "";
+                total_files_replaced = 0;
+                txtLog.Clear();
+
+                addLog("process started");
+
+                if (!CheckTextBoxes())
+                    return;
+
+                if (!CheckFolderExists())
+                    return;
+
+                if (!CheckFileExists())
+                    return;
+
+
+                target_folder = txtTargetFolder.Text;
+                target_file = txtTargetFile.Text;
+                file_name = GetFileNameFromFullPath(txtTargetFile.Text);
+
+                addLog("get files...");
+
+                List<string> files = Directory.GetFiles(target_folder, file_name, SearchOption.AllDirectories).ToList();
+
+                addLog("total file count to replace : " + files.Count);
+
+                addLog("listing paths...");
+                foreach (string path in files)
+                {
+                    addLog(path);
+
+                }
+
+                if (MessageBox.Show("Devam etmek istediğinize emin misiniz?", "UYARI",MessageBoxButtons.YesNo,MessageBoxIcon.Warning) == System.Windows.Forms.DialogResult.Yes)
+                {
+                    if (MessageBox.Show("Bak doğru söyle emin misin ? ", "UYARI", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == System.Windows.Forms.DialogResult.Yes)
+                    {
+                        addLog("process approved...");
+                    }
+                    else
+                    {
+                        addLog("process cancelled...");
+                        return;
+                    }
+                }
+                else
+                {
+                    addLog("process cancelled...");
+                    return;
+                }
+
+
+
+                foreach (string path in files)
+                {
+                    if (!ReplaceFile(path))
+                    {
+                        addLog("error replacing file : " + path);
+                    }
+                }
+                addLog("total files replaced : " + total_files_replaced);
+                addLog("operation completed...");
+
+            }
+            catch (Exception ex)
+            {
+                addLog("EXCEPTION = General error : " + ex.Message);
+            }
+            
+
+
+        }
+
+        private bool ReplaceFile(string path)
+        {
+            try
+            {
+                File.Copy(target_file, path,true);
+                total_files_replaced++;
+                addLog("file replaced : " + path);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                addLog("EXCEPTION = replaceFile exception : " + ex.Message);
+                return false;
+            }
+        }
+
+        private void addLog(string str)
+        {
+            txtLog.AppendText(str + "\n");
+        }
+
+        private bool CheckFolderExists()
+        {
+            addLog("checking folder exists...");
+
+            if (!Directory.Exists(txtTargetFolder.Text))
+            {
+                MessageBox.Show(txtTargetFolder.Text + " bulunamadı.");
+                return false;
+            }
+
+            return true;
+
+        }
+        
+        private bool CheckFileExists()
+        {
+            addLog("checking file exists...");
+
+            if (!File.Exists(txtTargetFile.Text))
+            {
+                MessageBox.Show(txtTargetFile.Text + " bulunamadı.");
+                return false;
+            }
+
+            return true;
+        }
+
+        private string GetFileNameFromFullPath(string path)
+        {
+            addLog("acquiring filename from full path...");
+            return Path.GetFileName(path);
+        }
+
+        private bool CheckTextBoxes()
+        {
+            addLog("checking textboxes...");
+            if (String.IsNullOrEmpty(txtTargetFolder.Text) || String.IsNullOrEmpty(txtTargetFile.Text))
+            {
+                MessageBox.Show("Hedef klasör ve hedef dosya alanlarını boş bırakamazsınız.");
+                return false;
+            }
+            return true;
+        }
+    }
+}
